2fb48d86fa547c7ffe1c97bee53878276d9d9da3
swarupoff27 <swarupoff27@gmail.com>
2022-07-14 18:06:38 +0530

    seventh js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const indicators = document.querySelectorAll('.card__indicator span');
 
 indicators.forEach((indicator, x) => {
   indicator.addEventListener('click', () => {
-    slider.style.transform = 'translateX(' + x * -460 + 'px)';
+    slider.style.transform = 'translateX(' + x * -20 + '%)';
     slider.style.transition = '1s ease';
   });
 });
@@ -29,7 +29,6 @@ const ind3 = document.getElementById('ind-3');
 const ind4 = document.getElementById('ind-4');
 const ind5 = document.getElementById('ind-5');
 
-
 function select_ind(indx, ind_a, ind_b, ind_c, ind_d) {
   document.getElementById(indx).classList.add('white');
 
@@ -39,68 +38,70 @@ function select_ind(indx, ind_a, ind_b, ind_c, ind_d) {
   document.getElementById(ind_d).classList.remove('white');
 }
 
-function select_card_color(testx, test_a, test_b, test_c, test_d) {
+function select_card(testx, test_a, test_b, test_c, test_d) {
   document.querySelector(testx).classList.add('green');
   document.querySelector(testx).classList.remove('white');
-
-  document.querySelector(test_a).classList.remove('green');
-  document.querySelector(test_a).classList.add('white');
-  document.querySelector(test_b).classList.remove('green');
-  document.querySelector(test_b).classList.add('white');
-  document.querySelector(test_c).classList.remove('green');
-  document.querySelector(test_c).classList.add('white');
-  document.querySelector(test_d).classList.remove('green');
-  document.querySelector(test_d).classList.add('white');
-
-}
-
-function select_text_color(testx, test_a, test_b, test_c, test_d) {
   document.querySelector(testx + ' img').src = 'images/whitequotes.png'
   document.querySelector(testx + ' p').style.color = 'white';
   document.querySelector(testx + ' :last-child').style.color = 'white';
 
+  document.querySelector(test_a).classList.remove('green');
+  document.querySelector(test_a).classList.add('white');
   document.querySelector(test_a +' img').src = 'images/greenquotes.png'
   document.querySelector(test_a +' p').style.color = '#031d5b';
   document.querySelector(test_a +' :last-child').style.color = '#031d5b';
+
+  document.querySelector(test_b).classList.remove('green');
+  document.querySelector(test_b).classList.add('white');
   document.querySelector(test_b +' img').src = 'images/greenquotes.png'
   document.querySelector(test_b +' p').style.color = '#031d5b';
   document.querySelector(test_b +' :last-child').style.color = '#031d5b';
+
+  document.querySelector(test_c).classList.remove('green');
+  document.querySelector(test_c).classList.add('white');
   document.querySelector(test_c +' img').src = 'images/greenquotes.png'
   document.querySelector(test_c +' p').style.color = '#031d5b';
   document.querySelector(test_c +' :last-child').style.color = '#031d5b';
+
+  document.querySelector(test_d).classList.remove('green');
+  document.querySelector(test_d).classList.add('white');
   document.querySelector(test_d +' img').src = 'images/greenquotes.png'
   document.querySelector(test_d +' p').style.color = '#031d5b';
   document.querySelector(test_d +' :last-child').style.color = '#031d5b';
+
 }
+
+function unselect_card(test_y){
+  document.querySelector(test_y).classList.remove('green');
+  document.querySelector(test_y).classList.add('white');
+  document.querySelector(test_y +' img').src = 'images/greenquotes.png'
+  document.querySelector(test_y +' p').style.color = '#031d5b';
+  document.querySelector(test_y +' :last-child').style.color = '#031d5b';
+}
+
 //================================================================================
 
 ind1.addEventListener('click', function () {
   select_ind('ind-1', 'ind-2', 'ind-3', 'ind-4', 'ind-5');
-  select_card_color('.test-1', '.test-2', '.test-3', '.test-4', '.test-5');
-  select_text_color('.test-1','.test-2', '.test-3', '.test-4', '.test-5');
+  select_card('.test-1', '.test-2', '.test-3', '.test-4', '.test-5');
 });
-
 
 ind2.addEventListener('click', function () {
   select_ind('ind-2', 'ind-1', 'ind-3', 'ind-4', 'ind-5');
-  select_card_color('.test-2', '.test-1', '.test-3', '.test-4', '.test-5');
-  select_text_color('.test-2','.test-1', '.test-3', '.test-4', '.test-5');
+  select_card('.test-2', '.test-1', '.test-3', '.test-4', '.test-5');
 });
 
 ind3.addEventListener('click', function () {
   select_ind('ind-3', 'ind-1', 'ind-2', 'ind-4', 'ind-5');
-  select_card_color('.test-3', '.test-1', '.test-2', '.test-4', '.test-5');
-  select_text_color('.test-3','.test-1', '.test-2', '.test-4', '.test-5');
+  select_card('.test-3', '.test-1', '.test-2', '.test-4', '.test-5');
 });
 
 ind4.addEventListener('click', function () {
   select_ind('ind-4', 'ind-1', 'ind-2', 'ind-3', 'ind-5');
-  select_card_color('.test-4', '.test-1', '.test-2', '.test-3', '.test-5');
-  select_text_color('.test-4','.test-1', '.test-2', '.test-3', '.test-5');
+  select_card('.test-4', '.test-1', '.test-2', '.test-3', '.test-5');
 });
 
 ind5.addEventListener('click', function () {
   select_ind('ind-5', 'ind-1', 'ind-2', 'ind-3', 'ind-4');
-  select_card_color('.test-5', '.test-1', '.test-2', '.test-3', '.test-4');
-  select_text_color('.test-5', '.test-1', '.test-2', '.test-3', '.test-4');
+  select_card('.test-5', '.test-1', '.test-2', '.test-3', '.test-4');
 });
